d74dacbe8988f6bb74e972da3e2bb900803f14ca
FersaLopez <321172974@alumno.enp.unam.mx>
2023-05-11 12:52:31 -0600

    Añadiendo Actividades

---
+++ Actividad 3/Sesion 4/ejemplosPresentacion.php
@@ -0,0 +1,188 @@
+<?php
+    //Constantes
+
+    define("CONSTANTE", "Que buena constante");
+    echo CONSTANTE;
+
+
+
+
+
+    echo "<br>";
+    echo "<br>";
+
+
+
+
+
+    const CURSO = "Web";
+    echo CURSO;
+
+
+    echo "<br>";
+    echo "<br>";
+       
+    
+    
+    //Tipos de Datos
+
+
+    //Bool
+    $variableOk = true;
+    $variableOknt = false;
+
+
+
+
+
+    //Entero
+    $soyEntero = 10;
+
+
+    //Nulo
+    $soyNulo = NULL;
+
+    //flotante
+    $soyFlotante = 1.777;
+
+
+
+
+
+    //Cadena (String)
+    $cadenita = 'MI PRIMERA PÁGINA WEB';
+
+
+
+
+    
+
+
+    
+
+    echo "<br>";
+    echo "<br>";
+
+    print("Que padre");
+    print "Que chido";
+
+
+
+
+    echo "<br>";
+    echo "<br>";
+    //print "<br>";
+
+
+    //print "Hola mucho gusto", "Soy Fersa";
+
+    echo "Hola mucho gusto", "Soy fersaaa";
+
+
+    echo "<br>";
+    echo "<br>";
+
+    var_dump("Ostras");
+    echo "<br>";
+    var_dump($soyFlotante);
+    echo "<br>";
+    var_dump($soyNulo, $soyEntero);
+    
+
+
+    echo "<br>";
+
+    $profesionDeDinero = "ingenieros";
+    $numeroPI = 3;
+
+
+
+
+    echo "Ellos son"." ".$profesionDeDinero;
+    echo "Ganan"." ".$numeroPI." pesos";
+
+
+
+
+
+    echo "<br>";
+    echo "<br>";
+
+
+
+
+
+
+
+    printf("El número PI segun los %s es %u", $profesionDeDinero, $numeroPI);
+
+
+
+/*
+
+    $a + $b
+
+    $a - $b
+
+    $a * $b
+
+    $a / $b
+
+    $a % $b
+
+    $a ** $b
+
+    +$a
+
+    -$a
+
+*/
+
+
+
+
+    //Soy un comentario de linea
+
+
+    /*
+        Soy un comentario de 
+        BLOQUE
+        Viva coyoroom
+    */
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    $a = 1;
+    $b = 2;
+    printf("%u", $a+$b);
+    printf("%u", $a+$b);
+    printf("%u", $a+$b);
+
+
+    $numero1 = 3;
+    $numero2 = 6;
+    
+    $resultado = 
+    echo $resultado;
+
+
+
+
+
+    $a += 2;    $a = $a
+
+
+
+
+
+
+
+    //
+
+
+
+?>
